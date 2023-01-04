fd28b8d2ec1c9f078a54cbe102f9948ad85872b8
Orest-Sabara <2626orestsabara@gmail.com>
2023-01-04 16:31:25 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import {products} from "./data/products";
 function App() {
     return (
         <div className='container mx-auto max-w-2xl pt-5'>
-            <Product product={products[0]}/>
-            <Product product={products[1]}/>
-            <Product product={products[2]}/>
+            {products.map(products => <Product product={products} key={products.id}/>)}
+
+            {/*<Product product={products[0]}/>*/}
+            {/*<Product product={products[1]}/>*/}
+            {/*<Product product={products[2]}/>*/}
         </div>
     );
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {IProduct} from "../models";
 
 interface ProductProps {
@@ -6,6 +6,11 @@ interface ProductProps {
 }
 
 export function Product(props: ProductProps) {
+    const [details, setDetails] = useState(false)
+
+    const btnClasses = details ? 'bg-blue-400' : 'bg-yellow-400'
+    const btnClass = ['py-2 px-4 border', btnClasses]
+
     return (
         <div
             className='border py-2 px-4 rounded flex flex-col items-center mb-2'
@@ -13,6 +18,17 @@ export function Product(props: ProductProps) {
             <img src={props.product.image} className='w-1/6' alt={props.product.title}/>
             <p>{props.product.title}</p>
             <p className='font-bold'>{props.product.price}</p>
+            <button
+                className={btnClass.join(' ')}
+                onClick={() => setDetails(prev => !prev)}
+            >
+                {details ? 'Hide details' : 'Show details'}
+            </button>
+            {details && <div>
+                <p>{props.product.description}</p>
+                <p>Rate: <span style={{fontWeight: 'bold'}}>{props.product.rating.rate}</span></p>
+            </div>}
+
         </div>
     )
 }
