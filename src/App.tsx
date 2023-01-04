4e3398008f62149501fd3c2946328a5dfa7cdaf1
Orest-Sabara <2626orestsabara@gmail.com>
2023-01-04 21:48:18 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
 import {Product} from "./components/Product";
-import {products} from "./data/products";
+import {useProducts} from "./hooks/useProducts";
+import {Loader} from "./components/Loader";
+import {ErrorMessage} from "./components/ErrorMessage";
 
 function App() {
+    const {products, error, loading} = useProducts()
+
     return (
         <div className='container mx-auto max-w-2xl pt-5'>
-            {products.map(products => <Product product={products} key={products.id}/>)}
-
-            {/*<Product product={products[0]}/>*/}
-            {/*<Product product={products[1]}/>*/}
-            {/*<Product product={products[2]}/>*/}
+            { loading && <Loader />}
+            { error &&  <ErrorMessage error={error}/>}
+            { products.map(products => <Product product={products} key={products.id}/>) }
         </div>
     );
 }
---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+import {IProduct} from "../models";
+import axios, {AxiosError} from "axios";
+
+export function useProducts () {
+    const [products, setProducts] = useState<IProduct[]>([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    async function fertchProducts() {
+        try {
+            setError('')
+            setLoading(true)
+            const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+            setProducts(response.data)
+            setLoading(false)
+        } catch (e: unknown) {
+            const error = e as AxiosError
+            setLoading(false)
+            setError(error.message)
+        }
+    }
+
+    useEffect(() => {
+        fertchProducts()
+    },[])
+
+    return {products, error, loading}
+}
