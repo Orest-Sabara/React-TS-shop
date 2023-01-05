737c385ea7eaf5f28ee9af177d0472fb7bacb740
Orest-Sabara <2626orestsabara@gmail.com>
2023-01-05 23:31:59 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
+import {Route, Routes} from "react-router-dom";
 import {Product} from "./components/Product";
-import {useProducts} from "./hooks/useProducts";
-import {Loader} from "./components/Loader";
-import {ErrorMessage} from "./components/ErrorMessage";
-import {Modal} from "./components/Modal";
-import {CreateProduct} from "./components/CreateProduct";
-import {useState} from "react";
-import {IProduct} from "./models";
+import {ProductsPage} from "./pages/ProductsPage";
+import {AboutPage} from "./pages/AboutPage";
+import {Navigations} from "./components/Navigations";
 
 function App() {
-    const {products, error, loading, addProduct} = useProducts()
-    const [modal, setModal] = useState(true)
-
-    const createHandler = (product: IProduct) => {
-        setModal(false)
-        addProduct(product)
-    }
-
     return (
-        <div>
-            <div className='container mx-auto max-w-2xl pt-5'>
-                { loading && <Loader />}
-                { error &&  <ErrorMessage error={error}/>}
-                { products.map(products => <Product product={products} key={products.id}/>) }
-                {modal && <Modal title='Create new product'>
-                    <CreateProduct onCreate={createHandler}/>
-                </Modal>}
-            </div>
-
-        </div>
-    );
+        <>
+            <Navigations />
+            <Routes>
+                <Route path='/' element={<ProductsPage/>}/>
+                <Route path='/about' element={<AboutPage/>}/>
+            </Routes>
+        </>
+    )
 }
 
 export default App;
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function AboutPage() {
+    return (
+        <>
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci, alias aliquam animi dolorem eos esse eum excepturi facilis impedit laudantium minus nam natus perspiciatis, quaerat saepe sunt, tempora vero.</div>
+
+        </>
+    )
+}
